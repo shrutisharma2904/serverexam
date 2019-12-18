4fb5108033c8f9f9c3105d4a3b8dfae0024fa1ec
Shruti <shruti@gmail.com>
2019-12-18 19:51:34 +0530

    exam

---
+++ schema.sql
@@ -0,0 +1,5 @@
+create table product(productId int,productName varchar(25),productDesc varchar(70),price float);
+insert into product values(1,'Bulb','electrical Product',562);
+insert into product values(2,'Mobile','electronics Product',562);
+insert into product values(3,'Wire','Electrical Product',562);
+insert into product values(4,'Charger','electronics Product',562);
